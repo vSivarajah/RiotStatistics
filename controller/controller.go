25e6c7dae2d37bd48a0e0cec18b7e26d32809387
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-07-08 20:15:58 +0200

    added CORS and binary file

--- controller/controller.go
+++ controller/controller.go
@@ -13,6 +13,7 @@ var apiKey = "RGAPI-404efd6b-e0a6-4194-bd44-7feb43a7efc0"
 func GetSummoner(c *gin.Context) {
 	summonerName := c.Param("name")
 	summonerProfile := summoner.GetSummonerDetails(summonerName)
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.JSON(http.StatusOK, summonerProfile)
 
 }
@@ -20,6 +21,7 @@ func GetSummoner(c *gin.Context) {
 func GetSummonerMatches(c *gin.Context) {
 	summonerName := c.Param("name")
 	summonerMatches := matches.GetMatches(summonerName)
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.JSON(http.StatusOK, summonerMatches)
 
 }
