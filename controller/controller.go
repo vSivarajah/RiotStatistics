ddc173531b0f6e4eaa86eb3e2708272e6df8c23c
Vignesh  Sivarajah <vigneshsivarajah@Vigneshs-MacBook-Pro.local>
2020-07-16 14:29:55 +0200

    error handling for not putting API KEY

--- controller/controller.go
+++ controller/controller.go
@@ -19,7 +19,10 @@ func GetSummoner(c *gin.Context) {
 	summonerName := c.Param("name")
 	client = api.NewClient(new(http.Client))
 	client.APIKey = os.Getenv("RIOTAPI_KEY")
-
+	if client.APIKey == "" {
+		c.JSON(http.StatusBadRequest, "API KEY not provided")
+		return
+	}
 	summonerProfile, err := client.Summoner.ByName(summonerName, "EUW1")
 
 	if err != nil {
@@ -34,6 +37,10 @@ func GetMatchesBySummonerId(c *gin.Context) {
 	summonerAccountId := c.Param("id")
 	client = api.NewClient(new(http.Client))
 	client.APIKey = os.Getenv("RIOTAPI_KEY")
+	if client.APIKey == "" {
+		c.JSON(http.StatusBadRequest, "API KEY not provided")
+		return
+	}
 	options := new(api.MatchListOptions)
 	minusTwoWeeks, err := time.ParseDuration("-336h")
 	if err != nil {
@@ -61,6 +68,10 @@ func GetPositionsBySummoner(c *gin.Context) {
 	summonerId := c.Param("id")
 	client = api.NewClient(new(http.Client))
 	client.APIKey = os.Getenv("RIOTAPI_KEY")
+	if client.APIKey == "" {
+		c.JSON(http.StatusBadRequest, "API KEY not provided")
+		return
+	}
 	data, restErr := client.League.PositionsBySummoner(summonerId, "EUW1")
 	fmt.Println(data)
 	if restErr != nil {
